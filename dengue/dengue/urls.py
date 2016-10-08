b4c4f539341240fb5f77342d5c1add14fb8a9162
Frank Yang <yangpoan@gmail.com>
2016-10-08 10:16:17 +0800

    app breeding api

--- dengue/dengue/urls.py
+++ dengue/dengue/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^users/', include('users.urls')),
+    url(r'^breeding_source/', include('breeding.urls')),
 ]
