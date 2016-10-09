23123157adaef910e6aa72b846f53740fbdf2c72
Frank Yang <yangpoan@gmail.com>
2016-10-09 16:48:58 +0800

    add table DengueBite & api /bite/

--- dengue/bite/urls.py
+++ dengue/bite/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from bite import views
 
 urlpatterns = [
+    url(r'^$', views.BiteCollection.as_view()),
 ]
--- dengue/bite/views.py
+++ dengue/bite/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import DengueBite
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class BiteCollection(APIView):
+
+    def post(self, request):
+        lng = request.data.get('lng', '')
+        lat = request.data.get('lat', '')
+
+        try:
+            lng = float(lng)
+            lat = float(lat)
+        except:
+            return Response({"detail": "經緯度錯誤"}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        bite = DengueBite(userprofile=request.user.userprofile,
+                          lng=lng,
+                          lat=lat)
+        bite.save()
+        return Response(status=status.HTTP_201_CREATED)
