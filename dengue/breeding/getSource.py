7c5b45cd7e47f72b01afafe8ac1e2d8c250b5497
HsuTing <hsuting0106@gmail.com>
2017-08-10 01:37:34 +0800

    add header and fix bug about description

--- dengue/breeding/getSource.py
+++ dengue/breeding/getSource.py
@@ -4,6 +4,7 @@ def run(output_file_path):
     data = Source.objects.filter(qualified_status='已通過')
 
     output_str_list = list()
+    output_str_list.append("姓名,電話,地址,描述")
     for item in data:
         if item.userprofile.phone == '':
             continue
@@ -11,7 +12,7 @@ def run(output_file_path):
         source_user_name = item.userprofile.name
         source_user_phone = item.userprofile.phone
         source_modified_address = item.modified_address
-        source_description = item.description
+        source_description = item.description.replace(',', '，', 10)
         output_str_item = "%s,%s,%s,%s" % \
             (source_user_name, source_user_phone, source_modified_address, source_description)
         output_str_list.append(output_str_item)
