8bfbe94b33ca7d7a299d284395cd7ebe8a49936f
LeeW <cl87654321@gmail.com>
2019-01-25 14:27:46 +0800

    Add docker-compose with postgis and redis

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd /dengue-backend
 
-python3 dengue/manage.py migrate --settings=dengue.settings.local
-python3 dengue/manage.py runserver 0.0.0.0:8000 --settings=dengue.settings.local
+python3 dengue/manage.py migrate --settings=dengue.settings.production
+python3 dengue/manage.py runserver 0.0.0.0:8000 --settings=dengue.settings.production
